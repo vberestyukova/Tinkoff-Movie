aa44c406cd52eb453178df6708565ef1389959e2
vberestyukova <berestyukova.vm@gmail.com>
2022-04-08 21:43:14 +0300

    Added loader and current year in footer

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {countOfSearch} from "./modules/countOfSearch.js";
 import {buttonHistoryFunc, buttonHistoryRemoveFunc, buttonMovieFunc} from "./modules/buttons.js";
 
 const elemResult = document.getElementById('movie_results');
-// const elem = document.querySelector('#search');
+
 if (localStorage.length === 0) {
     localStorage.setItem('movies_titles', ['Naruto', 'Titanic']);
 }
@@ -14,9 +14,13 @@ if (localStorage.length === 0) {
 let storeMovie = localStorage.getItem('movies_titles');
 storeMovie = storeMovie.split(',');
 const store = document.getElementById('history');
+
 createHistory(storeMovie, store);
 buttonHistoryFunc();
 buttonHistoryRemoveFunc();
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
 document.querySelector('input').addEventListener('keydown', async function(e) {
     if (e.key === "Enter") {
         while (store.firstChild) {
@@ -27,9 +31,17 @@ document.querySelector('input').addEventListener('keydown', async function(e) {
             elemResult.removeChild(elemResult.lastChild);
         }
         try {
+            const loader = document.querySelector('#no_results');
+            const loader_img = document.createElement("img");
+            loader_img.classList.add("loader");
+            loader_img.setAttribute("src", "dark-loader.gif");
+            loader.after(loader_img);
+
             const movie = await fetch(
                 `http://www.omdbapi.com/?s=${this.value}&apikey=65a5d248`
             ).then((r) => r.json());
+
+            loader_img.remove();
             movieCard(movie, elemResult);
             createHistory(storeMovie, store);
             countOfSearch(movie);
